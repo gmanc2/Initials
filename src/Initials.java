ef05431a120eb3f39a4d06d1805856864fda4fc6
Glenn <gwallacec2@gmail.com>
2020-02-10 22:29:29 -0500

    Added

---
+++ src/Initials.java
@@ -0,0 +1,18 @@
+/*
+ * Initials
+ * Program Description - Display initials
+ * Written by Glenn Wallace
+ * 2/10/2020
+ * JDK 1.8
+ */
+public class Initials {
+    public static void main(String[] args) {
+        System.out.println("G G G G G G G       W           W           W                                     ");
+        System.out.println("G                    W         W W         W                                      ");
+        System.out.println("G                     W       W   W       W                                       ");
+        System.out.println("G   G G G G G G        W     W     W     W                                        ");
+        System.out.println("G           G           W   W       W   w                                         ");
+        System.out.println("G           G            W W         W W                                          ");
+        System.out.println("G G G G G G G             W           W                                           ");
+    }
+}
